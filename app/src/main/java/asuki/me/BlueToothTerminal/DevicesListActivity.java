d119a35c20f29712afc4591559df76a633563cbd
Asuki <changes729@163.com>
2018-07-10 13:37:04 +0800

    Change TextView Text color

--- app/src/main/java/asuki/me/BlueToothTerminal/DevicesListActivity.java
+++ app/src/main/java/asuki/me/BlueToothTerminal/DevicesListActivity.java
@@ -8,6 +8,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +20,8 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -30,152 +34,150 @@ import java.util.UUID;
  */
 
 public class DevicesListActivity extends Activity {
+  private BluetoothAdapter bluetoothAdapter;
+  private List<String> mDevicesArray = new ArrayList<String>();
+  private DevicesListAdapter<String> devicesListAdapter;
+  private List<BluetoothDevice> deviceList = new ArrayList<BluetoothDevice>();
 
-    private ProgressBar progressbarSearchDevices;
+  /**
+   * @brief:
+   * @param: savedInstanceState
+   * @retval:
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-    private BluetoothAdapter mBluetoothAdapter;
-    private List<String> mDevicesArray = new ArrayList<String>();
-    private DevicesListAdapter<String> devicesListAdapter;
-    private List<BluetoothDevice> deviceList = new ArrayList<BluetoothDevice>();
+    setContentView(R.layout.listview_devices);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    ListView listView = findViewById(R.id.listview_devices);
+    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        setContentView(R.layout.listview_devices);
+    // 将已配对的设备添加到列表中
+    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+    if (pairedDevices.size() > 0) {
+      for (BluetoothDevice device : pairedDevices) {
+        mDevicesArray.add(device.getName() + "\n" + device.getAddress());
+        deviceList.add(device);
+      }
+    }
+    // 注册广播接收器，以获取蓝牙设备搜索结果
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+    registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+    // 搜索蓝牙设备
+    bluetoothAdapter.startDiscovery();
 
-        ListView listView = (ListView) findViewById(R.id.listview_devices);
-        progressbarSearchDevices = (ProgressBar) findViewById(R.id.progressbar_search_devices);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    // 为ListView控件设置适配器
+    devicesListAdapter = new DevicesListAdapter<String>(getApplicationContext(), mDevicesArray);
+    listView.setAdapter(devicesListAdapter);
+  }
 
-        // 将已配对的设备添加到列表中
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                mDevicesArray.add(device.getName() + "\n" + device.getAddress());
-                deviceList.add(device);
-            }
-        }
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    unregisterReceiver(mReceiver);
+  }
 
+  // Create a BroadcastReceiver for ACTION_FOUND
+  private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      // When discovery finds a device
+      if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+        // Get the BluetoothDevice object from the Intent
+        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        deviceList.add(device);
+        // Add the name and address to an array adapter to show in a ListView
+        mDevicesArray.add(device.getName() + "\n" + device.getAddress());
+        Toast.makeText(getApplicationContext(), device.getName() + "\n" + device.getAddress(), Toast.LENGTH_SHORT).show();
+        devicesListAdapter.notifyDataSetChanged();
+      }
+    }
+  };
 
-        // 注册广播接收器，以获取蓝牙设备搜索结果
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-        // 搜索蓝牙设备
-        mBluetoothAdapter.startDiscovery();
-        progressbarSearchDevices.setVisibility(View.VISIBLE);
+  class DevicesListAdapter<T> extends BaseAdapter {
+    Context context;
+    List<T> list;
+    private LayoutInflater inflater;
 
-        // 为ListView控件设置适配器
-        devicesListAdapter = new DevicesListAdapter<String>(getApplicationContext(), mDevicesArray);
-        listView.setAdapter(devicesListAdapter);
+    class Holder{
+      TextView deviceName;
+    }
 
+    DevicesListAdapter(Context context, List list){
+      this.context = context;
+      this.list = list;
+      inflater = LayoutInflater.from(context);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mReceiver);
+    public int getCount() {
+      return list.size();
     }
 
-    // Create a BroadcastReceiver for ACTION_FOUND
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                deviceList.add(device);
-                // Add the name and address to an array adapter to show in a ListView
-                mDevicesArray.add(device.getName() + "\n" + device.getAddress());
-                Toast.makeText(getApplicationContext(), device.getName() + "\n" + device.getAddress(), Toast.LENGTH_SHORT).show();
-                devicesListAdapter.notifyDataSetChanged();
-            }
-        }
-    };
-
-    class DevicesListAdapter<T> extends BaseAdapter {
-
-        Context context;
-        List<T> list;
-        private LayoutInflater inflater;
-        public DevicesListAdapter(Context context, List list){
-            this.context = context;
-            this.list = list;
-            inflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public T getItem(int position) {
-            return list.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            Holder holder;
-            if(convertView==null){
-                holder = new Holder();
-                convertView = inflater.inflate(R.layout.item_listview_devices, null);
-                holder.deviceName = (TextView) convertView.findViewById(R.id.item_device_name);
-                convertView.setTag(holder);
-            }else{
-                holder = (Holder) convertView.getTag();
-            }
-            holder.deviceName.setText(list.get(position).toString());
-            holder.deviceName.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    BluetoothDevice device = deviceList.get(position);
-            BluetoothSocket socket = null;
-            try {
-                // 蓝牙串口服务对应的UUID。如使用的是其它蓝牙服务，需更改下面的字符串
-                UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-                socket = device.createRfcommSocketToServiceRecord(MY_UUID);
-            } catch (Exception e) {
-                Log.d("log", "获取Socket失败");
-                Toast.makeText(getApplicationContext(), "获取Socket失败", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            mBluetoothAdapter.cancelDiscovery();
-            try {
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
-                socket.connect();
-                Log.d("log", "连接成功");
-                Toast.makeText(getApplicationContext(), "连接成功", Toast.LENGTH_SHORT).show();
-                BluetoothUtils.setBluetoothSocket(socket);
-                progressbarSearchDevices.setVisibility(View.INVISIBLE);
-
-                // 连接成功，返回主界面
-                finish();
-            } catch (IOException connectException) {
-                // Unable to connect; close the socket and get out
-                Log.d("log", "连接失败");
-                try {
-                    socket.close();
-                } catch (IOException closeException) { }
-                return;
-            }
-
-
-                }
-            });
-            return convertView;
-        }
-
-        protected class Holder{
-            TextView deviceName;
-        }
-
+    @Override
+    public T getItem(int position) {
+      return list.get(position);
     }
+
+    @Override
+    public long getItemId(int position) {
+      return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+      Holder holder;
+      if(null == convertView) {
+        convertView = inflater.inflate(R.layout.item_listview_devices, null);
+        holder = new Holder();
+        holder.deviceName = convertView.findViewById(R.id.item_device_name);
+        convertView.setTag(holder);
+      } else {
+        holder = (Holder) convertView.getTag();
+      }
+
+      holder.deviceName.setText(list.get(position).toString());
+      holder.deviceName.setTextColor(getBaseContext().getResources().getColorStateList(R.color.text_Black));
+      holder.deviceName.setTextSize(16);
+      holder.deviceName.setPadding(40,20,60,20);
+
+      holder.deviceName.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          BluetoothDevice device = deviceList.get(position);
+          BluetoothSocket socket;
+          try {
+            // 蓝牙串口服务对应的UUID。如使用的是其它蓝牙服务，需更改下面的字符串
+            UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+            socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+          } catch (Exception e) {
+            Log.d("log", "获取Socket失败");
+            Toast.makeText(getApplicationContext(), "获取Socket失败", Toast.LENGTH_SHORT).show();
+            return;
+          }
+
+          bluetoothAdapter.cancelDiscovery();
+          try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            socket.connect();
+            Log.d("log", "连接成功");
+            Toast.makeText(getApplicationContext(), "连接成功", Toast.LENGTH_SHORT).show();
+            BluetoothUtils.setBluetoothSocket(socket);
+
+            finish();
+          } catch (IOException connectException) {
+            // Unable to connect; close the socket and get out
+            Log.d("log", "连接失败");
+            try {
+              socket.close();
+            } catch (IOException closeException) { }
+            return;
+          }
+        }
+      });
+      return convertView;
+    }
+  }
 }
